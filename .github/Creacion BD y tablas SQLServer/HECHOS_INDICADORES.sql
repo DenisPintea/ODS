86e3fa2ec912418692c0a0d3851745bf08ee65a8
DenisPintea <constantin.pintea.denis@alumnos.upm.es>
2025-06-22 00:05:33 +0200

    TABLA DE HECHOS
    Cambiar el tipo a integer para el id_fact_indicador

--- .github/Creacion BD y tablas SQLServer/HECHOS_INDICADORES.sql
+++ .github/Creacion BD y tablas SQLServer/HECHOS_INDICADORES.sql
@@ -5,7 +5,7 @@ GO
 
 CREATE TABLE L3.hechos_indicadores (
     id_hecho INT IDENTITY(1,1) PRIMARY KEY,		--(PK) clave primaria subrogada de la tabla
-    id_fact_indicador VARCHAR(10) NOT NULL,		--(PFK) clave foranea primaria de dim_indicador
+    id_fact_indicador INT NOT NULL,		--(PFK) clave foranea primaria de dim_indicador
     id_fact_calendario INT NOT NULL,			--(PFK) clave foranea primaria de dim_calendario
     id_fact_autonomia INT NOT NULL,				--(PFK) clave foranea primaria de dim_autonomia
     valor DECIMAL(15,3),						--medida del indicador
