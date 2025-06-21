d5eb0e89b98836d3211b51ada58fb298b45065b5
DenisPintea <constantin.pintea.denis@alumnos.upm.es>
2025-06-21 17:54:41 +0200

    DIM_AUTONOMIAS
    Se ha corregido la tabla para que tenga una clave autoincremental y subrogada

--- .github/Creacion BD y tablas SQLServer/DIM_AUTONOMIAS.sql
+++ .github/Creacion BD y tablas SQLServer/DIM_AUTONOMIAS.sql
@@ -3,12 +3,13 @@ GO
 -- Crear la tabla autonomias
 --drop table ods.dim_autonomias
 CREATE TABLE ods.dim_autonomias (
-   ID_Autonomia INT IDENTITY(1,1) PRIMARY KEY,  -- Columna autoincremental
-   Nombre_provincia VARCHAR(100)       -- Columna para el nombre de la provincia
+   id_tabla INT IDENTITY(1,1) PRIMARY KEY,			-- Clave autoincremental
+   id_autonomia INT NOT NULL UNIQUE,				-- Columna subrogada
+   nombre_autonomia VARCHAR(100) NOT NULL,			-- Columna para el nombre de la provincia
 );
 
 -- Insertar los registros en orden alfabético sin acentos ni tildes
-INSERT INTO dim_autonomias (id_autonomia, nombre_autonomia)
+INSERT INTO ods.dim_autonomias (id_autonomia, nombre_autonomia)
 VALUES
 (1, 'Andalucia'),
 (2, 'Aragon'),
