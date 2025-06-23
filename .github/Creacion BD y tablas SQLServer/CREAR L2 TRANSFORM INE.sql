152c6f96b458fa2e952248edab792cb0e89df6f9
DenisPintea <constantin.pintea.denis@alumnos.upm.es>
2025-06-23 19:37:56 +0200

    L2 separar en dos archivos FAO e INE
    Se han separado los sql script al igual que se hizo con los de L1

--- .github/Creacion BD y tablas SQLServer/CREAR L2 TRANSFORM INE.sql
+++ .github/Creacion BD y tablas SQLServer/CREAR L2 TRANSFORM INE.sql
@@ -52,16 +52,3 @@ CREATE TABLE [L2].L2_INE_2A2_AOD_BRUTO
 , Valor DECIMAL(10,3)
 )
 ;
-
---TABLAS FAO:
-
---1)CREAR TABLA L2_FAO_ODS_2C1
-CREATE TABLE [L2].L2_FAO_ODS_2C1
-(
-  id_L2_fao_ods_2c1 BIGINT PRIMARY KEY IDENTITY(1,1)
-, id_indicador INT
-, id_calendario INT
-, id_autonomia INT
-, IPC DECIMAL(10,3)
-)
-;
